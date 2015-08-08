92ba7e4e94762723fb327074794c955b879e5524
kaluluosi111 <kaluluosi@gmail.com>
2015-08-08 12:13:28 +0800

    提交AvalonDock的MVVM微框架

---
+++ MVVMDialog/ConsoleApplication1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Class1
+    {
+        static Class1() {
+            Console.WriteLine("哈哈");
+            Console.Read();
+        }
+    }
+}
---
+++ MVVMDialog/MvvmLight1/View/PaneTemplate/Relation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvvmLight1.View.PaneTemplate
+{
+    public class Relation
+    {
+        public Type ViewModelType { get; set; }
+        public Type ViewType { get; set; }
+    }
+}
